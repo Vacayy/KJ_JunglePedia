8e85eabcc7258d54831f9c0670f5d5f57cce6afe
Vacayy <125976021+Vacayy@users.noreply.github.com>
2023-10-12 21:09:50 +0900

    Add files via upload

---
+++ backend/app.py
@@ -0,0 +1,83 @@
+from flask import Flask, request, jsonify, render_template
+from pymongo import MongoClient
+import pandas as pd
+import logging
+import jwt
+from module import (login, logout, create_post, get_my_posts, delete_post, 
+                    like_post, update_post, view_most_liked_posts, view_recent_posts)
+from init_db import initialize_db
+from module.signup import signup as signup_func
+
+app = Flask(__name__, template_folder='/Users/juyeongkim/Desktop/Software/Jungle/week0_miniProject/brainforest/frontend/templates')
+
+####################################
+# 로거 생성 및 설정
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)  # 로그 레벨 설정
+
+# 콘솔 출력 핸들러 설정
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+# 로그 포맷 설정
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+
+# 핸들러를 로거에 추가
+logger.addHandler(ch)
+####################################
+
+# JWT 적용
+SECRET_KEY = 'your_secret_key'  # JWT 토큰과 Flask 앱 설정에 사용될 SECRET_KEY
+
+@app.route('/api/v1/verify_token', methods=['POST'])
+def verify_token():
+    token = request.json.get('token')
+
+    if not token:
+        return jsonify({'result': 'fail', 'message': '토큰이 제공되지 않았습니다.'}), 400
+
+    try:
+        decoded_payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        return jsonify({'result': 'success', 'message': '토큰이 유효합니다.', 'payload': decoded_payload}), 200
+
+    except jwt.ExpiredSignatureError:
+        return jsonify({'result': 'fail', 'message': '토큰이 만료되었습니다.'}), 401
+    except jwt.InvalidTokenError:
+        return jsonify({'result': 'fail', 'message': '유효하지 않은 토큰입니다.'}), 401
+
+app.config['SECRET_KEY'] = SECRET_KEY  # Flask 앱 설정에 SECRET_KEY 설정
+
+# MongoDB 설정 및 데이터베이스 연결을 앱의 컨텍스트에 저장
+client = MongoClient("mongodb://localhost:27017/")
+db = client["jungle_db"]
+users_collection = db["USERS"]
+posts_collection = db["POSTS"]
+likes_collection = db["LIKES"]
+
+@app.route('/api/v1/signup', methods=['POST'])
+def signup_route():
+    return signup_func()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "페이지를 찾을 수 없습니다. 요청한 URL이 잘못되었을 수 있습니다.", 404
+
+# 블루프린트 등록
+app.register_blueprint(login.login_bp)
+app.register_blueprint(logout.logout_bp)
+app.register_blueprint(create_post.create_post_bp)
+app.register_blueprint(get_my_posts.get_my_posts_bp)
+app.register_blueprint(delete_post.delete_post_bp)
+app.register_blueprint(like_post.like_post_bp)
+app.register_blueprint(update_post.update_post_bp)
+app.register_blueprint(view_most_liked_posts.view_most_liked_posts_bp)
+app.register_blueprint(view_recent_posts.view_recent_posts_bp)
+
+if __name__ == '__main__':
+    initialize_db()
+    app.run(debug=True, threaded=False)
--- backend/module/signup.py
+++ backend/module/signup.py
@@ -1,25 +1,45 @@
-from flask import Blueprint, request, jsonify
+from flask import request, jsonify, render_template
 from werkzeug.security import generate_password_hash
+from pymongo import MongoClient
 
-signup_bp = Blueprint('signup', __name__)
+# MongoDB 설정
+client = MongoClient("mongodb://localhost:27017/")
+db = client["junglePedia1"]
+users_collection = db["USERS"]
+posts_collection = db["POSTS"]
+likes_collection = db["LIKES"]
 
-@signup_bp.route('/api/v1/signup', methods=['POST'])
-def signup(users_collection, allowed_users):
-    user_id = request.json.get('id')  # 'email'을 'id'로 변경
+
+def signup():
+    user_id = request.json.get('id')
     password = request.json.get('password')
     name = request.json.get('name')
 
-    if users_collection.find_one({"id": user_id}):  # 'email'을 'id'로 변경
-        return jsonify({"status": "failure", "message": "아이디가 이미 존재합니다"}), 400  # 메시지 수정
+    # 필수 데이터 확인
+    if not all([user_id, password, name]):
+        return jsonify({"status": "failure", "message": "아이디, 비밀번호, 이름을 모두 입력해주세요."}), 400
 
-    if not any(user['아이디'] == user_id for user in allowed_users):  # '이메일'을 '아이디'로 변경
-        return jsonify({"status": "failure", "message": "등록할 수 없는 아이디입니다"}), 403  # 메시지 수정
+    # 사용자가 목록에 있는지 확인
+    existing_user = users_collection.find_one({"id": user_id})
+    if not existing_user:
+        return jsonify({"status": "failure", "message": "등록할 수 없는 아이디입니다. 정글 입학시 사용한 이메일을 입력해주세요."}), 403
+    
+    if existing_user.get("isRegistered", False): # 'isRegistered' 필드 값이 True일 경우 실행
+        return jsonify({"status": "failure", "message": "이미 등록된 사용자입니다."}), 403
 
-    hashed_pw = generate_password_hash(password)
-    users_collection.insert_one({
-        "id": user_id,  # 'email'을 'id'로 변경
-        "password": hashed_pw,
-        "name": name
-    })
+    # 이름 일치 여부 확인
+    if existing_user["name"] != name:
+        return jsonify({"status": "failure", "message": "이름이 일치하지 않습니다."}), 403
 
-    return jsonify({"status": "success", "message": "회원가입 성공"}), 201
+    # 비밀번호를 'scrypt' 방식으로 해시화
+    hashed_pw = generate_password_hash(password, method='scrypt')
+    
+    # 사용자 데이터 업데이트
+    users_collection.update_one(
+        {"id": user_id},
+        {"$set": {"password": hashed_pw, "isRegistered": True}}
+    )
+    
+    print("(가입) 이름:", name, "/ 이메일:", user_id,"/ 비밀번호:", password)
+    return jsonify({"status": "success", "message": "회원가입 성공"}), 200
+
